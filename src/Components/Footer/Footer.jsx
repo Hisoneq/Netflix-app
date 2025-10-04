9d41d1fc8bb0ec83434a671eb1e7c49940182de9
Raman <watuwnnt@gmail.com>
2025-10-04 17:29:32 +0300

    add: footer links

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,21 @@
 import styles from "./Footer.module.css"
 
 export default function Footer(){
+
+    const footerLinks = [
+        { link: "FAQ", href: "#" },
+        { link: "Help Center", href: "#" },
+        { link: "Account", href: "#" },
+        { link: "Media Center", href: "#" },
+        { link: "Investor Relations", href: "#" },
+        { link: "Jobs", href: "#" },
+        { link: "Ways to Watch", href: "#" },
+        { link: "Terms of Use", href: "#" },
+        { link: "Privacy", href: "#" },
+        { link: "Cookie Preferences", href: "#" },
+        { link: "Corporate Information", href: "#" },
+        { link: "Contact Us", href: "#" },
+    ]
     return(
         <footer className={styles.footer}>
             <p className={styles.footerText}>Ready to watch? Enter your email to create or restart your membership.</p>
@@ -9,6 +24,14 @@ export default function Footer(){
                     <input type="email" placeholder="Email address" />
                     <button className={styles.button}>Get Started →</button>
             </div>
+
+            <ul className={styles.footerLinks}>
+                {footerLinks.map((link) => (
+                    <li key={link.link}>
+                        <a href={link.href}>{link.link}</a>
+                    </li>
+                ))}
+            </ul>
         </footer>
     )
 }
