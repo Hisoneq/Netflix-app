47bd99f028dff66f93155ca6372a78f03eb56d6c
Raman <watuwnnt@gmail.com>
2025-10-05 15:18:14 +0300

    add: stiles for login page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -11,7 +11,23 @@ export default function Login(){
                 width={200}
                  />
 
-            <h1>Login</h1>
+            <div className={styles.loginForm}>
+                <h1>Sign In</h1>
+                <input type="email" placeholder="Email or phone number" />
+                <input type="password" placeholder="Password" />
+                <button>Sign In</button>
+                <p className={styles.or}>OR</p>
+                <button className={styles.signInCodeButton}>Use a Sign-in code</button>
+                <a href="#" className={styles.forgotPassword}>Forgot your password?</a>
+                <div className={styles.checkboxContainer}>
+                    <input type="checkbox" id="remember" />
+                    <label htmlFor="remember">Remember me</label>
+                </div>
+                <div className={styles.signupContainer}>
+                    <p className={styles.newToNetflix}>New to Netflix?</p>   
+                    <a className={styles.signUpButton} href="#">Sign up now.</a>
+                </div>
+            </div>
         </div>
     )
 }
